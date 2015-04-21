700b14d6dea0d348d6f3fb275a1aa2c2a97734ca
q2apro.com <info@q2apro.com>
2015-04-21 15:24:19 +0300

    Create qa-recent-events-widget-lang-default.php

---
+++ qa-recent-events-widget-lang-default.php
@@ -0,0 +1,28 @@
+<?php
+/*
+	Question2Answer Plugin: Recent Events Widget
+	License: http://www.gnu.org/licenses/gpl.html
+*/
+
+	return array(
+		'recent_events' => 'Recent Events',
+		'question' => 'question',
+		'new_question' => 'New Question',
+		'new_question_abbr' => 'Q',
+		'new_answer' => 'New Answer',
+		'new_answer_abbr' => 'A',
+		'new_comment' => 'New Comment',
+		'new_comment_abbr' => 'C',
+		'new_bestanswer' => 'Best Answer',
+		'new_bestanswer_abbr' => 'B',
+		'new_by' => 'by', // new x BY member x
+		'anonymous' => 'anonymous', // new x BY member x
+		'hour_indic' => 'h', // will appear behind time, e.g. 06:45h
+		'new_user_abbr' => 'R',
+		'new_user' => 'New User'
+	);
+	
+
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
