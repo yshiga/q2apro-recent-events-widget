8af158f505e465936a68df907a79a55bc3f1debf
yshiga <yuichi.shiga@gmail.com>
2015-06-05 16:55:08 +0900

    add ja lang

---
+++ qa-recent-events-widget-lang-ja.php
@@ -0,0 +1,33 @@
+<?php
+/*
+	Question2Answer Plugin: Recent Events Widget
+	License: http://www.gnu.org/licenses/gpl.html
+*/
+
+	return array(
+		'recent_events' => '最近の投稿',
+		'question' => '質問',
+		'new_question' => '新しい質問',
+		'new_question_abbr' => '質問',
+		'new_answer' => '新しい回答',
+		'new_answer_abbr' => '回答',
+		'new_comment' => '新しいコメント',
+		'new_comment_abbr' => 'コメント',
+		'new_bestanswer' => 'ベストアンサー',
+		'new_bestanswer_abbr' => 'ベストアンサー',
+		'new_by' => 'by', // new x BY member x
+		'anonymous' => '匿名ユーザー', // new x BY member x
+		'hour_indic' => '', // will appear behind time, e.g. 06:45h
+		'new_user_abbr' => 'ユーザー登録',
+		'new_user' => '新しいユーザー',
+		'ago' => '前', 
+
+		// for setting
+		'admin_count' => 'count', 
+		'admin_time_format' => 'time format', 
+	);
+	
+
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
